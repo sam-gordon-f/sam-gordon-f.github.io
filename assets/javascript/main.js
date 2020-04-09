3fba1420e2ea5a3f28b0750eaf3017d0641aa5b9
sam gordon <sam.gordon.f@gmail.com>
2020-04-09 16:03:10 +1000

    quick formatting test

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,23 @@
-var arrayA = ["A"]
-var arrayW = ["whimsical", "wasted", "wistful"]
-var arrayS = ["screename"]
+var arrayA = ["A."];
+var arrayW = ["Whimsical.", "Wasted.", "Wistful."];
+var arrayS = ["Screename."];
 
 var rotateBrand = function(index) {
-    // reset index if at end
-  if(arrayW.length == index)
-    index = 0;
-
-  var textWTemp = arrayW[index];
-  
-  $("#h1HeadingMain").fadeOut(1000).text(textWTemp).fadeIn(1000)
-  setTimeout(rotateBrand(index++), 5000);
+  // var textWTemp = textWPrevious = "";
+  //
+  //   // loop logic
+  // if(arrayW.length == index) {
+  //   indexTemp = 0;
+  //   textWPrevious = arrayW[(arrayW.length - 1)]
+  //
+  // } else {
+  //   indexTemp = index;
+  //   textWPrevious = arrayW[indexTemp - 1)]
+  // }
+  // textWTemp = arrayW[index];
+  //
+  // $("#h1HeadingMain").fadeOut(1000).text(textWTemp).fadeIn(1000)
+  // setTimeout(rotateBrand(indexTemp++), 5000);
 }
 
 $(document).ready(function() {

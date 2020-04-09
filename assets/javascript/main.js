a9434688110082090afe517fa9f247095821ce82
sam gordon <sam.gordon.f@gmail.com>
2020-04-09 16:07:49 +1000

    simplifying brand rotate

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,21 +3,13 @@ var arrayW = ["Whimsical.", "Wasted.", "Wistful."];
 var arrayS = ["Screename."];
 
 var rotateBrand = function(index) {
-  // var textWTemp = textWPrevious = "";
-  //
-  //   // loop logic
-  // if(arrayW.length == index) {
-  //   indexTemp = 0;
-  //   textWPrevious = arrayW[(arrayW.length - 1)]
-  //
-  // } else {
-  //   indexTemp = index;
-  //   textWPrevious = arrayW[indexTemp - 1)]
-  // }
-  // textWTemp = arrayW[index];
-  //
-  // $("#h1HeadingMain").fadeOut(1000).text(textWTemp).fadeIn(1000)
-  // setTimeout(rotateBrand(indexTemp++), 5000);
+  
+    // loop logic
+  if(arrayW.length == index)
+    indexTemp = 0;
+  
+  $("#spanW").fadeOut(1000).text(arrayW[index]).fadeIn(1000)
+  setTimeout(rotateBrand(indexTemp++), 10000);
 }
 
 $(document).ready(function() {

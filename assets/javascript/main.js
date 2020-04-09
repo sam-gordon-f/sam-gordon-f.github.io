2c9f8114aa4329163101bd9559aa4749ae89a57b
sam gordon <sam.gordon.f@gmail.com>
2020-04-09 14:59:20 +1000

    testing rotate with message

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   // rotate the main banner
   
   while(true) {
-    $("#h1HeadingMain").fadeOut(400).delay(1000).fadeIn()
+    $("#h1HeadingMain")
+      .fadeOut(400)
+      .delay(1000)
+      .fadeIn(function() {
+        console.log('switched')
+      })
   }
 });
